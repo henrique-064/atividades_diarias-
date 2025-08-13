59dad952d6f00e401748392dc3cd0e05000192d6
henrique-064 <adriahenrique064@gmail.com>
2025-08-13 17:53:00 -0300

    mais um dia de estudos, 13/08/2025, ontem teve algumas complicações, por isso sem arquivos novos

--- atividade_processamento_de_API_frutas-11-08-2025.py
+++ atividade_processamento_de_API_frutas-11-08-2025.py
@@ -1,5 +1,5 @@
 import requests
-from requisições.lista_requests import requisitar_get , testar_api
+from requisições.lista_requests import requisitar_get, testar_api
 
 url = "https://securitylogs.pythonanywhere.com/frutas"
 
@@ -8,12 +8,13 @@ resultado = requisitar_get(url)
 if resultado and "data" in resultado:
     for item in resultado["data"]:
         fruta = item.get("fruta", {})
-        print(f"Nome: {fruta.get('nome', 'N/A')}")
-        print(f"Cor: {fruta.get('cor', 'N/A')}")
-        print(f"Origem: {fruta.get('origem', 'N/A')}")
-        print(f"Tipo: {fruta.get('tipo', 'N/A')}")
-        print(f"Vitamina Principal: {fruta.get('vitamina_principal', 'N/A')} \n")
+        print(f"Nome: {fruta.get('nome')}")
+        print(f"Cor: {fruta.get('cor')}")
+        print(f"Origem: {fruta.get('origem')}")
+        print(f"Tipo: {fruta.get('tipo')}")
+        print(f"Vitamina Principal: {fruta.get('vitamina_principal')} \n")
 else:
     print("Nenhum dado encontrado ou erro na requisição.")
 
-print(testar_api(url))
+teste = testar_api(url)
+print(teste)
